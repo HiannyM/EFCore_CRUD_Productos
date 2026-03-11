d944ade00b642703ed2ffa8d8aeee5d6355c2de2
HiannyM <angelich2001@gmail.com>
2026-03-11 18:52:05 -0400

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EFCore_CRUD.Models.Productos.Models;
+using EFCore_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_CRUD.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Producto> Productos { get; set; }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;    
+
+namespace EFCore_CRUD.Models.Productos.Models;
+
+public class Producto
+{
+    public int Id { get; set; }
+    [Required]
+    public string Nombre { get; set; }
+    [Precision(10, 2)]
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+}
